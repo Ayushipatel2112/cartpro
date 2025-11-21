87cff091b2d0232af02cf5cb7e894844e633299a
Ayushi <dalsaniyauma@gmail.com>
2025-11-21 22:34:15 +0530

    Refactor using directives and add login message

--- login.aspx.cs
+++ login.aspx.cs
@@ -3,3 +3,13 @@ using System.Web.UI;
 using System.Configuration;
 using System.Data.SqlClient;
 namespace CartProWebApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack && Request.QueryString["msg"] == "registered")
+            {
+                lblMessage.Text = "Registration successfully. Please login.";
+            }
+        }
